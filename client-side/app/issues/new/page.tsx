648190a8172bbe9661aecaaa2fa053f821fbcd4e
darlegui <arleguidaniel@gmail.com>
2024-02-14 02:24:48 -0800

    Add additional skeletons

--- client-side/app/issues/new/page.tsx
+++ client-side/app/issues/new/page.tsx
@@ -1,18 +1,16 @@
 'use client';
-import { IssueType } from '@/app/api/IssueService';
-import axios from 'axios';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Callout, TextField, TextFieldInput } from '@radix-ui/themes';
+import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { POST } from '@/app/api/issues/route';
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false });
 
 type IssueForm = z.infer<typeof createIssueSchema>;
