66c70c61b4cda1e7554e5e84533a20f32550795f
darlegui <arleguidaniel@gmail.com>
2024-02-17 23:22:59 -0800

    Added Prisma Adapter

---
+++ client-side/prisma/migrations/20240218060357_re_adjusted_tables/migration.sql
@@ -0,0 +1,46 @@
+/*
+  Warnings:
+
+  - You are about to drop the `google_user` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `account` DROP FOREIGN KEY `Account_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `session` DROP FOREIGN KEY `Session_userId_fkey`;
+
+-- DropTable
+DROP TABLE `google_user`;
+
+-- DropTable
+DROP TABLE `users`;
+
+-- CreateTable
+CREATE TABLE `app_users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_name` VARCHAR(15) NOT NULL,
+    `password` BLOB NOT NULL,
+
+    UNIQUE INDEX `id`(`id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `emailVerified` DATETIME(3) NULL,
+    `image` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
