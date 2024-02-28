0a7f6e3c76c999c9f2da11b0218672eac773aeb3
darlegui <arleguidaniel@gmail.com>
2024-02-27 21:16:42 -0800

    Build the LatestIssue component

--- client-side/app/page.tsx
+++ client-side/app/page.tsx
@@ -1,10 +1,11 @@
+import LatestIssues from './LatestIssues';
 import Pagination from './components/Pagination';
 
 export default function Home({ searchParams }: { searchParams: { page: string } }) {
   return (
     <>
       {/* <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />; */}
-      <div>Hello World</div>
+      <LatestIssues />
     </>
   );
 }
