3d6ddf64e6e663e9ebcb475be57ca6a0cb3deabb
darlegui <arleguidaniel@gmail.com>
2024-03-02 09:38:13 -0800

    Build IssueChat Component

--- client-side/app/page.tsx
+++ client-side/app/page.tsx
@@ -3,6 +3,7 @@ import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
 import Pagination from './components/Pagination';
 import { Flex, Grid } from '@radix-ui/themes';
+import IssueChart from './IssueChart';
 
 export default async function Home({ searchParams }: { searchParams: { page: string } }) {
   const open = await prisma.issues.count({ where: { status: 'OPEN' } });
@@ -12,7 +13,7 @@ export default async function Home({ searchParams }: { searchParams: { page: str
     <Grid columns={{ initial: '1', md: '2' }}>
       {/* <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />; */}
       <Flex direction="column" gap="5">
-        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
         <LatestIssues />
       </Flex>
     </Grid>
