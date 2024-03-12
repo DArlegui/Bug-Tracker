df9b0c601f20e887ab40af49ddd938ab74c4aa28
darlegui <arleguidaniel@gmail.com>
2024-03-12 03:30:18 -0700

    Update API_URL assignment in environment.ts

--- client-side/environment.ts
+++ client-side/environment.ts
@@ -1,1 +1,10 @@
-export const API_URL = `https://${process.env.BACKEND_URL}`;
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Check if BACKEND_URL is populated correctly
+console.log('API_URL:', process.env.NEXT_PUBLIC_BACKEND_URL);
+
+// Assign API_URL
+export const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
